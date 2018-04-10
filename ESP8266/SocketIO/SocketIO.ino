2472de20e5134ab4d894d2392dba89336bc4f728
chung-official <chung3396@gmail.com>
2018-04-10 17:22:18 +0700

    Update ESP8266/SocketIO/SocketIO.ino
    Thêm phân loại Device theo deviceType, position
    
    Chuyển GET sang POST
    
    Fix lỗi không kết nối lại khi Server ngắt và khởi động lại

--- ESP8266/SocketIO/SocketIO.ino
+++ ESP8266/SocketIO/SocketIO.ino
@@ -5,7 +5,7 @@
  
 SocketIOClient client;
 ESP8266WiFiMulti wifimulti;
-HTTPClient httpGet;
+HTTPClient httpClient;
 
 int yellow = D8;
 int green = D7;
@@ -13,27 +13,25 @@ int red = D6;
 
 int LEDs[] = {D8, D7, D6, D5, D4, D3, D2, D1, D0};
 
-//char host[] = "192.168.1.6";
 char host[] = "172.16.199.170";    
 int port = 3000;                  
 
 extern String RID; // tên sự kiện
 extern String Rfull; // json
-String GET_packet = "";
  
 unsigned long prevTime = 0;
 long interval = 5000;
 
-void setupNetwork() {
+void setupNetwork()
+{
     WiFi.persistent(false);
     WiFi.mode(WIFI_STA);
 //    wifimulti.addAP("BON FPT", "kemtuchon19k");
 //    wifimulti.addAP("BON VNPT", "kemtuchon19k");
 //    wifimulti.addAP("FPT Telecom", "dongthap");
-//    wifimulti.addAP("AnhTraiMua", "meochonhe");
+    wifimulti.addAP("AnhTraiMua", "meochonhe");
     wifimulti.addAP("HoangPhat_Pro", "20052010");
-//    wifimulti.addAP("ANDY", "01666988779");
-
+    wifimulti.addAP("ANDY", "01666988779");
 
     uint8_t i = 0;
     while (wifimulti.run() != WL_CONNECTED && i++ < 20) delay(500);
@@ -41,107 +39,130 @@ void setupNetwork() {
         Serial.println("Can not connect wifi!");
         while (1) delay(500);
     }
-    Serial.print("Wifi connected to ");    Serial.println(WiFi.SSID());
+    Serial.print("Wifi connected to ");    
+    Serial.println(WiFi.SSID());
     
-    Serial.print("ESP8266 IP: ");    Serial.println(WiFi.localIP());
+    Serial.print("ESP8266 IP: ");    
+    Serial.println(WiFi.localIP());
 }
 
 void changeState(int pin, boolean st)
 {
-    if (st) {
-      digitalWrite(pin, HIGH);
-      Serial.println("Light on!");
+    if (st) 
+    {
+        digitalWrite(pin, HIGH);
+        Serial.println("Light on!");
     } 
-    else {
-      digitalWrite(pin, LOW);
-      Serial.println("Light off !");
+    else 
+    {
+        digitalWrite(pin, LOW);
+        Serial.println("Light off !");
     }
+    client.send("d2s-change",Rfull);
+    
 }
 
-void Light(String device, boolean st)
-{
-  if (device == "Light 1")
-        changeState(LEDs[0], st);
-  if (device == "Light 2")
-        changeState(LEDs[1], st);
-  if (device == "Light 3")
-        changeState(LEDs[2], st);
-  if (device == "Light 4")
-        changeState(LEDs[3], st);
-  if (device == "Light 5")
-        changeState(LEDs[4], st);
-  if (device == "Light 6")
-        changeState(LEDs[5], st);
-  if (device == "Light 7")
-        changeState(LEDs[6], st);
-  if (device == "Light 8")
-        changeState(LEDs[7], st);
-  if (device == "Light 9")
-        changeState(LEDs[8], st);
-}
-
-void Filter(JsonObject& obj)
-{
-    const char* deviceName = obj["deviceName"];
-    boolean state = obj["state"];
-        Light(deviceName, state);
+void Light(String device, String position, boolean st)
+{   
+    Serial.println(device);
+    Serial.println(position);
+    Serial.println(st);
+    if(position == "LIVINGROOM")
+    {
+        if (device == "Light 1")
+            changeState(LEDs[0], st);
+        else if (device == "Light 2")
+            changeState(LEDs[1], st);
+    }
+    else if(position == "BEDROOM")  
+    {
+        if (device == "Light 1")
+            changeState(LEDs[0], st);
+        else if (device == "Light 2")
+            changeState(LEDs[1], st);
+    }
+    else if(position == "DININGROOM") 
+    {
+        if (device == "Light 1")
+            changeState(LEDs[0], st);
+        else if (device == "Light 2")
+            changeState(LEDs[1], st);
+    }
+    else if(position == "BATHROOM") 
+    {
+        if (device == "Light")
+        {
+            changeState(LEDs[0], st);
+        }
+    }
 }
 
 void parseJsonArray(String s)
 {
-  DynamicJsonBuffer bufferred(512);
-  JsonArray& arr = bufferred.parseArray(s);
-  if (arr.success())
-  {
-    for (int i = 0; i < arr.size(); i++)
-    {
-      JsonObject& object = arr[i];
-      Filter(object);
+    DynamicJsonBuffer bufferred(512);
+    JsonArray& arr = bufferred.parseArray(s);
+    if (arr.success())
+        for (int i = 0; i < arr.size(); i++)
+        {
+            JsonObject& object = arr[i];
+            Filter(object);
+        }
+    else Serial.println("parsing failed!!!");
+}
+
+void Filter(JsonObject& obj)
+{
+    String deviceName = obj["deviceName"];
+    String deviceType = obj["deviceType"];
+    String position = obj["position"];
+    boolean state = obj["state"];
+    if(deviceType == "LIGHT")
+        Light(deviceName, position, state);
+}
+
+
+void listenSocketIO()
+{
+    if (RID != "")
+    {        
+        StaticJsonBuffer<512> jsonBuffer;
+        JsonObject& obj = jsonBuffer.parseObject(Rfull);
+        if (obj.success())
+        {
+            Filter(obj);         
+        }
+        else Serial.println("Parsing failed!!!");
     }
-  }
-  else Serial.println("parsing failed!!!");
+    else Serial.println("Parsing...");
 }
 
-void checkMSG()
-{
-  if (RID != "")
-  {        
-      StaticJsonBuffer<512> jsonBuffer;
-      JsonObject& obj = jsonBuffer.parseObject(Rfull);
-      if (obj.success())
-      {
-          client.send("d2s-change",Rfull);
-          Filter(obj);
-      }
-      else Serial.println("Parsing failed!!!");
-  }
-  else 
-  {
-    Serial.println("Parsing...");
-  }
-}
-
-bool GETfromServer(String field)
-{
-    bool check = false;
+void loadDeviceByType(String deviceType)
+{ 
     String serverAdd = host;
-    httpGet.begin("http://" + serverAdd + ":3000/device/" + field);
-    if (httpGet.GET())
-      {
-        GET_packet = httpGet.getString();   
-        Serial.println(GET_packet); 
-        parseJsonArray(GET_packet);
-        check = true;
-      }
-    GET_packet = "";
-    httpGet.end();
-    return check;
+    Serial.println("Get POST from http://" + serverAdd + ":3000/devices/"); 
+    httpClient.begin("http://" + serverAdd + ":3000/devices/");
+    
+    httpClient.addHeader("Content-Type", "application/json");
+
+    String query = "{\"deviceType\":\"" + deviceType + "\"}";
+    Serial.println("With query " + query);   
+     
+    int httpCode = httpClient.POST(query);
+    Serial.println(httpCode);   //Print HTTP return code
+    
+    if(httpCode == 200){
+        String payload = httpClient.getString();
+        Serial.println(payload);   
+        parseJsonArray(payload);
+    }
+    httpClient.end();
 }
 
 void LEDsetup()
 {
-  for (int i = 0; i < 9; i++)
-    pinMode(LEDs[i], OUTPUT);
+    for (int i = 0; i < 9; i++){
+        pinMode(LEDs[i], OUTPUT);
+    }
 }
 
 void setup()
@@ -151,23 +172,18 @@ void setup()
     setupNetwork();
     client.connect(host, port);
     Serial.println("connected to server!");
-//    client.send("c2s-ledchange","{\"id\":\"5ac5c52688102a039f6b925e\",\"deviceName\":\"Light 3\",\"position\":\"LIVINGROOM\",\"state\":1,\"connect\":false}");
-//    delay(1000);
-//    client.send("c2s-ledchange","{\"id\":\"5ac5c52688102a039f6b925e\",\"deviceName\":\"Light 3\",\"position\":\"LIVINGROOM\",\"state\":0,\"connect\":false}");
-    GETfromServer("lights");
-    GETfromServer("doors");
+    loadDeviceByType("DOOR");
+    loadDeviceByType("LIGHT");
 }
 
 void loop()
 {
-  if (client.monitor())
-  {
-    Serial.println(RID);
-    Serial.println(Rfull);
-    checkMSG();
+    if (client.monitor())
+    {
+        listenSocketIO(); 
+    }
     if (!client.connected())
     {
-      client.reconnect(host, port);
+        client.reconnect(host, port);
     }
-  }
 }
